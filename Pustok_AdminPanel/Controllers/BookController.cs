ce1c24fef0ba7825f6efb8393702272184166004
validash <100703058+validash@users.noreply.github.com>
2022-06-16 15:31:26 +0400

    book modal detail added

---
+++ Pustok_AdminPanel/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetBookModal(int id)
+        {
+            Book book = _context.Books.Include(x => x.Genre).Include(x => x.Author).Include(x => x.BookImages).FirstOrDefault(x => x.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return PartialView("_BookModalPartial", book);
+        }
+    }
+}
---
+++ Pustok_AdminPanel/wwwroot/assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $(".show-detail").click(function (e) {
+        e.preventDefault();
+        let url = $(this).attr("href");
+        fetch(url).then(response => {
+            if (!response.ok) {
+                alert("Xeta bas verdi")
+                return;
+            }
+            return response.text()
+        })
+            .then(data => {
+                if (data) {
+                    $("#bookDetailModal .modal-content").html(data);
+                    $("#bookDetailModal").modal('show');
+                }
+            })
+   })
+})
